6476c7de1df9216e2bd1ca73ffc77e04edaa957e
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-31 15:38:23 -0400

    weekend changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://brosen@localhost/noteful'
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://brosen@localhost/noteful-test'
+  DB_URL: process.env.DATABASE_URL || 'postgresql://brosen@localhost/noteful',
+  TEST_DB_URL:
+    process.env.TEST_DB_URL || 'postgresql://brosen@localhost/noteful-test'
 };
